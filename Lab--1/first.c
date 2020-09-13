08413daf7f0c4fc4dec7f1604996634963d633d8
sethvs <sethvs@users.noreply.github.com>
2020-09-13 16:58:18 +0300

    Lab--1

---
+++ Lab--1/first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Автор: Васин С.В
+
+main(int args, char *argv[])
+{
+  printf("Автор: Васин С.В.\n\n");
+  printf("Sex & drugs & Rock-n-Roll!!!\n");
+  printf("It is all you need!!!\n");
+  printf("\nPress Enter!\n");
+  
+  getchar();
+}
