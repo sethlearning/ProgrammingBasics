db1e7b98074d9153db53a8e28d78bcb6591f53aa
sethvs <sethvs@users.noreply.github.com>
2021-06-14 14:46:16 +0300

    Cleanup 1

--- Lab-3a/lists.c
+++ Lab-3a/lists.c
@@ -11,20 +11,6 @@ void fillInListAuto(struct thelist *, int);
 void fillInListManual(struct thelist *, int);
 void countNegatives(struct thelist *, int *, int *);
 
-
-
-
-/*
-void ExitProgram(char*);
-
-void DefineArrayManual(int*, int);
-void DefineArrayAuto(int*, int);
-void PrintArray(int*, int);
-void CountNegatives(int*, int, int*, int*);
-*/
-
-
-
 int main(int argc, char* argv[])
 {
     char *configFileName = "lists.conf";
@@ -87,62 +73,10 @@ int main(int argc, char* argv[])
     // negatives
     countNegatives(alist, &ncount, &nsum);
     printf("Количество отрицательных элементов: %i \nИх сумма: %i\n\n", ncount, nsum);
+
     // destroy
     destroy(alist);
 
-/*
-    int count, negativesCount=0, negativesSumma=0, i;
-    char mode[7], example[150];
-
-    logo();
-    printf("Лабораторная работа №3.\nПодсчет количества отрицательных чисел и их суммы.\n\n");
-    snprintf(example, 200, "Пример использования:\n%s NumberOfElements=number ArrayEnteringMode=auto/manual", argv[0]);
-
-
-    if (argc < 3)
-    {
-        printf("Недостаточно параметров.\n");
-        ExitProgram(example);
-    }
-
-    for (i = 1; i < argc; i++)
-    {
-        if (strstr(argv[i], "ArrayEnteringMode="))
-            strcpy(mode, argv[i] + 18);
-
-        if (strstr(argv[i], "NumberOfElements="))
-            count = atoi(argv[i] + 17);
-    }
-
-    if (count < 2)
-    {
-        printf("Неверные параметры.\n");
-        ExitProgram(example);
-    }
-    printf("Количество элементов: %d\n", count);
-
-    int array[count];
-    if(!strcmp(mode, "manual"))
-    {
-        printf("Метод ввода массива: %s\n\n", mode);
-        DefineArrayManual(array, count);
-    }
-    else if (!strcmp(mode, "auto"))
-    {
-        printf("Метод ввода массива: %s\n\n", mode);
-        DefineArrayAuto(array, count);
-    }
-    else
-    {
-        printf("Неверные параметры.\n");
-        ExitProgram(example);
-    }
-
-    PrintArray(array, count);
-    CountNegatives(array, count, &negativesCount, &negativesSumma);
-
-    printf("Количество отрицательных элементов: %d\nСумма отрицательных элементов: %d\n\n", negativesCount, negativesSumma);
-*/
 }
 
 void processConfig(FILE *config, int *autof, int *size)
